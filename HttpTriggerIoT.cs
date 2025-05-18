ff9555033bacd5f75cf7633ced9a731fc6f1fb96
rjacomep <70068976+rjacomep@users.noreply.github.com>
2025-05-17 22:05:19 -0500

    Update HttpTriggerIoT.cs

--- HttpTriggerIoT.cs
+++ HttpTriggerIoT.cs
@@ -1,7 +1,7 @@
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Net;
 
 namespace iothubandroid.Function
 {
@@ -15,10 +15,14 @@ namespace iothubandroid.Function
         }
 
         [Function("HttpTriggerIoT")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteString("Welcome to Azure Functions!");
+            return response;
         }
     }
 }
