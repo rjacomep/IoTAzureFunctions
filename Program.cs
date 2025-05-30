df546a6929ea05d4c587bac4b3a8c61ef1fa54e6
DavidLLopez <david.lopez@niceactimize.com>
2025-05-30 04:01:50 -0500

    registro del blobserviceclient

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection; // <-- ESTA ES LA LÍNEA CLAVE
+using Azure.Storage.Blobs;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
+// REGISTRO DEL BlobServiceClient
+builder.Services.AddSingleton(x =>
+{
+  string? connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+
+if (string.IsNullOrWhiteSpace(connectionString))
+    throw new InvalidOperationException("La cadena de conexión 'AzureWebJobsStorage' no está configurada.");
+
+var blobServiceClient = new BlobServiceClient(connectionString);
+
+    return new BlobServiceClient(connectionString);
+});
 
 builder.Build().Run();
+
